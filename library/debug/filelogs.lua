760c8777fea866c422c1f9a7dada7ba00eb964b0
x2Neptune <x2neptunereal@gmail.com>
2025-04-01 23:47:36 +0700

    Update filelogs.lua

--- library/debug/filelogs.lua
+++ library/debug/filelogs.lua
@@ -1,24 +1,31 @@
 local debug_number = tostring(math.floor(os.clock()) * math.random(1000,9999))
 
 local debugFunction = function(func : any)
-    local filename = "alchemy_debugs/" .. os.date('%A, %B %d %Y').." (".. debug_number ..").txt"
-    if not isfolder('alchemy_debugs') then
-        makefolder('alchemy_debugs')
-    end
-
-    local x,p = pcall(func)
-    if p then
-        if isfile(filename) then
-            appendfile(filename, "\n[" .. tostring(math.floor(tick())) .. "] ".. p)
-        else
-            writefile(filename,"Starts at : "..os.date('%A, %B %d %Y').." / code : ".. debug_number .. "\nThis file was create by Alchemy Debug System, send this file for report bugs.\n")
-            appendfile(filename, "\n[" .. tostring(math.floor(tick())) .. "] ".. p)
-
-            print("[ ALCHEMY DEBUG ] Create debug file \"".. filename .."\"")
+    if isfolder and makefolder and isfile and writefile and appendfile then
+        local filename = "alchemy_debugs/" .. os.date('%A, %B %d %Y').." (".. debug_number ..").txt"
+        if not isfolder('alchemy_debugs') then
+            makefolder('alchemy_debugs')
         end
 
-        print("[ ALCHEMY DEBUG ] " .. p)
-    end;
+        local x,p = pcall(func)
+        if p then
+            if isfile(filename) then
+                appendfile(filename, "\n[" .. tostring(math.floor(tick())) .. "] ".. p)
+            else
+                writefile(filename,"Starts at : "..os.date('%A, %B %d %Y').." / code : ".. debug_number .. "\nThis file was create by Alchemy Debug System, send this file for report bugs.\n")
+                appendfile(filename, "\n[" .. tostring(math.floor(tick())) .. "] ".. p)
+
+                print("[ ALCHEMY DEBUG ] Create debug file \"".. filename .."\"")
+            end
+
+            print("[ ALCHEMY DEBUG ] " .. p)
+        end;
+    else
+        local x,p = pcall(func)
+        if p then
+            print("[ ALCHEMY DEBUG ] " .. p)
+        end
+    end
 end
 
 return debugFunction
