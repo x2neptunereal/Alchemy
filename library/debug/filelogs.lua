5fcd0f56c5591adf9a2ff78528deb22aba5fcb75
x2Neptune <x2neptunereal@gmail.com>
2025-04-04 17:58:17 +0700

    Update filelogs.lua

--- library/debug/filelogs.lua
+++ library/debug/filelogs.lua
@@ -1,10 +1,24 @@
-local debug_number = tostring(math.floor(os.clock()) * math.random(1000,9999))
+local getTimeOfTheDay = function(uni : string)
+    local seconds = tick() % 86400
+    local hours = math.floor(seconds / 3600)
+    local minutes = math.floor((seconds % 3600) / 60)
+    local secs = math.floor(seconds % 60)
+    return string.format("%d".. uni .."%02d".. uni .."%02d", hours, minutes, secs)
+end
+
+local getLocalTime = function()
+    return tostring(math.floor(tick()))
+end
+
+local getDate = function()
+    return os.date('%A, %B %d %Y')
+end
 
 local debugFunction = function(func : any, name : string)
     local func_name = name or "nil"
 
     if isfolder and makefolder and isfile and writefile and appendfile then
-        local filename = "alchemy_debugs/" .. os.date('%A, %B %d %Y').." (".. debug_number ..").txt"
+        local filename = "alchemy_debugs/" .. getDate().." (".. getTimeOfTheDay("-") ..").txt"
 
         if not isfolder('alchemy_debugs') then
             makefolder('alchemy_debugs')
@@ -13,10 +27,10 @@ local debugFunction = function(func : any, name : string)
         local x,p = pcall(func)
         if p then
             if isfile(filename) then
-                appendfile(filename, "\n[" .. tostring(math.floor(tick())) .. ":".. func_name .."] ".. p)
+                appendfile(filename, "\n[" .. getLocalTime() .. ":".. func_name .."] ".. p)
             else
-                writefile(filename,"Starts at : "..os.date('%A, %B %d %Y').." / code : ".. debug_number .. "\nThis file was create by Alchemy Debug System, send this file for report bugs.\n")
-                appendfile(filename, "\n[" .. tostring(math.floor(tick())) .. ":".. func_name .."] ".. p)
+                writefile(filename,"Starts at : "..getDate().." / ".. getTimeOfTheDay(":") .. "\nThis file was create by Alchemy Debug System, send this file for report bugs.\n")
+                appendfile(filename, "\n[" .. getLocalTime() .. ":".. func_name .."] ".. p)
 
                 print("[ ALCHEMY DEBUG ] Create debug file \"".. filename .."\"")
             end
