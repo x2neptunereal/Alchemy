5bb5835786fe29b529e6c6aaf273b6967830424d
x2Neptune <x2neptunereal@gmail.com>
2025-04-07 21:06:30 +0700

    Update filelogs.lua

--- library/debug/filelogs.lua
+++ library/debug/filelogs.lua
@@ -19,30 +19,37 @@ local fileTime = getTimeOfTheDay("-")
 local debugFunction = function(func : any, name : string)
     local func_name = name or "nil"
 
-    if isfolder and makefolder and isfile and writefile and appendfile then
-        local filename = "alchemy_debugs/" .. getDate().." (".. fileTime ..").txt"
-
-        if not isfolder('alchemy_debugs') then
-            makefolder('alchemy_debugs')
-        end
-
+    if diableFile then
         local x,p = pcall(func)
         if p then
-            if isfile(filename) then
-                appendfile(filename, "\n[" .. getLocalTime() .. ":".. func_name .."] ".. p)
-            else
-                writefile(filename,"Starts at : "..getDate().." / ".. getTimeOfTheDay(":") .. "\nThis file was create by Alchemy Debug System, send this file for report bugs.\n")
-                appendfile(filename, "\n[" .. getLocalTime() .. ":".. func_name .."] ".. p)
+            print("[ ALCHEMY DEBUG : ".. func_name .." ] " .. p)
+        end
+    else
+        if isfolder and makefolder and isfile and writefile and appendfile then
+            local filename = "alchemy_debugs/" .. getDate().." (".. fileTime ..").txt"
 
-                print("[ ALCHEMY DEBUG ] Create debug file \"".. filename .."\"")
+            if not isfolder('alchemy_debugs') then
+                makefolder('alchemy_debugs')
             end
 
-            print("[ ALCHEMY DEBUG : ".. func_name .." ] " .. p)
-        end;
-    else
-        local x,p = pcall(func)
-        if p then
-            print("[ ALCHEMY DEBUG : ".. func_name .." ] " .. p)
+            local x,p = pcall(func)
+            if p then
+                if isfile(filename) then
+                    appendfile(filename, "\n[" .. getLocalTime() .. ":".. func_name .."] ".. p)
+                else
+                    writefile(filename,"Starts at : "..getDate().." / ".. getTimeOfTheDay(":") .. "\nThis file was create by Alchemy Debug System, send this file for report bugs.\n")
+                    appendfile(filename, "\n[" .. getLocalTime() .. ":".. func_name .."] ".. p)
+
+                    print("[ ALCHEMY DEBUG ] Create debug file \"".. filename .."\"")
+                end
+
+                print("[ ALCHEMY DEBUG : ".. func_name .." ] " .. p)
+            end;
+        else
+            local x,p = pcall(func)
+            if p then
+                print("[ ALCHEMY DEBUG : ".. func_name .." ] " .. p)
+            end
         end
     end
 end
