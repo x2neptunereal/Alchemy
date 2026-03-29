f776b8d5db08711b7aadbf2488e21cdad2b2a32d
x2Neptune <x2neptunereal@gmail.com>
2026-03-29 21:02:57 +0700

    Create list.lua

---
+++ library/advertising/list.lua
@@ -0,0 +1,6 @@
+return = {
+    "https://raw.githubusercontent.com/x2neptunereal/Alchemy/refs/heads/main/image/robloxads/exampleAds.png",
+    -- "https://raw.githubusercontent.com/x2neptunereal/Alchemy/refs/heads/main/image/robloxads/exampleAds.png",
+    -- "https://raw.githubusercontent.com/x2neptunereal/Alchemy/refs/heads/main/image/robloxads/exampleAds.png",
+    -- "https://raw.githubusercontent.com/x2neptunereal/Alchemy/refs/heads/main/image/robloxads/exampleAds.png",
+}
