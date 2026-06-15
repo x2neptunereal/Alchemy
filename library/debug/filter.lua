9c5a8b0e4782ef331f908cfec649ded8cd596b74
x2Neptune <x2neptunereal@gmail.com>
2026-06-15 11:09:17 +0700

    Update filter.lua

--- library/debug/filter.lua
+++ library/debug/filter.lua
@@ -5,6 +5,8 @@ return {
   {--CONTAIN ERROR
     " 285",
     " 277",
+    "reset HWID",
+    "Whitelist",
     "script_key",
     "VALID key or NOT USE it at all",
     "for this key, specified by script owner. Wait 5 minutes",
